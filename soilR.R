988fd3ce742c132e01c24443e360c1470499dd4b
mmlaan <maggi.laan@gmail.com>
2025-04-01 10:55:23 -0700

    add CAF No-Till model

--- soilR.R
+++ soilR.R
@@ -1,8 +1,10 @@
-install.packages("SoilR")
+#install.packages("SoilR")
 
 library(SoilR)
 library(knitr)
 
+
+## Example from Spohn and Sierra 2018
 tau = seq(0,1000) #vector of transit times
 
 a = seq(0,1000) # vector of ages
@@ -24,3 +26,26 @@ Pa$meanSystemAge
 Pa$meanPoolAge
 
 Ptt$meanTransitTime
+
+## CAF No-Till ####
+
+#col 1 veg
+#col 2 mineral
+
+uP = matrix(c(0,0.27), 2, 1) # external inputs (deposition, no fixation or weathering)
+
+xP = matrix(c(1.27, 1140), 2,2, byrow = TRUE) # steady state contents of pools (plant biomass, litter layer, soil)
+
+FP = matrix(c(-6.975, 13.95,6.975,-0.27 -13.95), byrow = TRUE, 2,2)
+
+BP = FP/xP
+
+BP
+
+Pa = systemAge(BP, uP, a)
+
+Ptt = transitTime(BP, uP, tau)
+
+Pa$meanSystemAge
+
+Pa$meanPoolAge
